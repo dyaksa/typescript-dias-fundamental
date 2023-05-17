071a601661d33ed78f1b7b4dded39b2958c60aed
dyaksaa <diasnour0395@gmail.com>
2023-05-17 16:25:50 +0700

    start

---
+++ bundle/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let namaSaya = 'Dyaksa Jauharuddin';
+let isNikah = true;
+let pacarSaya = 'Putri Oktaviani';
+console.log({ namaSaya });
+console.log({ isNikah });
+console.log({ pacarSaya });
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let namaSaya: string = 'Dyaksa Jauharuddin';
+let isNikah: boolean = true;
+let pacarSaya: string = 'Putri Oktaviani';
+
+console.log({ namaSaya });
+console.log({ isNikah });
+console.log({ pacarSaya });
