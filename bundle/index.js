ce4b35325ad98007705dc45e5a1f96280570064c
dyaksaa <diasnour0395@gmail.com>
2023-05-17 19:20:37 +0700

    first commit

--- bundle/index.js
+++ bundle/index.js
@@ -2,6 +2,29 @@
 let namaSaya = 'Dyaksa Jauharuddin';
 let isNikah = true;
 let pacarSaya = 'Putri Oktaviani';
+// array
+let isHobi;
+isHobi = ['Mendaki', 'Gunung', 'Balapan'];
+// tuple array
+let isNotHobi;
+isNotHobi = ['Mendaki', 1, true];
+let isTeman;
+isTeman = {
+    nama: 'Alfa',
+    umur: 25,
+    isNikah: true,
+};
+//! Function
+function create() {
+    return 1;
+}
+// arrow function
+const update = () => 'success update';
+console.log(create());
+console.log(update());
 console.log({ namaSaya });
 console.log({ isNikah });
 console.log({ pacarSaya });
+console.log({ isHobi });
+console.log({ isNotHobi });
+console.log({ isTeman });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,42 @@ let namaSaya: string = 'Dyaksa Jauharuddin';
 let isNikah: boolean = true;
 let pacarSaya: string = 'Putri Oktaviani';
 
+// array
+let isHobi: string[];
+isHobi = ['Mendaki', 'Gunung', 'Balapan'];
+
+// tuple array
+let isNotHobi: [string, number, boolean];
+isNotHobi = ['Mendaki', 1, true];
+
+//! tipe data custom
+type Teman = {
+  nama: string;
+  umur: number;
+  isNikah: boolean;
+};
+
+let isTeman: Teman;
+isTeman = {
+  nama: 'Alfa',
+  umur: 25,
+  isNikah: true,
+};
+
+//! Function
+function create(): number {
+  return 1;
+}
+
+// arrow function
+const update = (): string => 'success update';
+
+console.log(create());
+console.log(update());
+
 console.log({ namaSaya });
 console.log({ isNikah });
 console.log({ pacarSaya });
+console.log({ isHobi });
+console.log({ isNotHobi });
+console.log({ isTeman });
