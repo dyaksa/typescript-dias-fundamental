2eb9b6689d388b64cea4f12369511d7c156b127a
dyaksaa <diasnour0395@gmail.com>
2023-05-19 09:39:02 +0700

    interface

--- bundle/index.js
+++ bundle/index.js
@@ -20,6 +20,12 @@ function create() {
 }
 // arrow function
 const update = () => 'success update';
+// func parameter
+const add = (x, y) => {
+    return `${x} ditambah ${y} hasilnya ${x + y}`;
+};
+const result = add(30, 20);
+console.log(result);
 console.log(create());
 console.log(update());
 console.log({ namaSaya });
@@ -28,3 +34,27 @@ console.log({ pacarSaya });
 console.log({ isHobi });
 console.log({ isNotHobi });
 console.log({ isTeman });
+const createIntel = (processor) => {
+    console.log(processor.brand);
+    console.log(processor.baseModel);
+};
+const createAMD = (processor) => {
+    console.log(processor.brand);
+    console.log(processor.baseModel);
+};
+const intel = {
+    brand: 'Intel',
+    baseModel: 'Core i7',
+    modelName: 'Core i7-10700K',
+    clockSpeed: 3.8,
+    turboBoost: true,
+};
+const ryzen = {
+    brand: 'AMD',
+    baseModel: 'Ryzen 7',
+    modelName: 'Ryzen 7 5800X',
+    clockSpeed: 3.8,
+    precisionBoost: true,
+};
+createIntel(intel);
+createAMD(ryzen);
--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,14 @@ function create(): number {
 // arrow function
 const update = (): string => 'success update';
 
+// func parameter
+const add = (x: number, y: number): string => {
+  return `${x} ditambah ${y} hasilnya ${x + y}`;
+};
+
+const result: string = add(30, 20);
+console.log(result);
+
 console.log(create());
 console.log(update());
 
@@ -41,3 +49,48 @@ console.log({ pacarSaya });
 console.log({ isHobi });
 console.log({ isNotHobi });
 console.log({ isTeman });
+
+//! Interface
+interface IProcessor {
+  brand: string;
+  baseModel: string;
+  modelName: string;
+  clockSpeed: number;
+}
+
+interface Intel extends IProcessor {
+  turboBoost: boolean;
+}
+
+interface AMD extends IProcessor {
+  precisionBoost: boolean;
+}
+
+const createIntel = (processor: Intel): void => {
+  console.log(processor.brand);
+  console.log(processor.baseModel);
+};
+
+const createAMD = (processor: AMD): void => {
+  console.log(processor.brand);
+  console.log(processor.baseModel);
+};
+
+const intel: Intel = {
+  brand: 'Intel',
+  baseModel: 'Core i7',
+  modelName: 'Core i7-10700K',
+  clockSpeed: 3.8,
+  turboBoost: true,
+};
+
+const ryzen: AMD = {
+  brand: 'AMD',
+  baseModel: 'Ryzen 7',
+  modelName: 'Ryzen 7 5800X',
+  clockSpeed: 3.8,
+  precisionBoost: true,
+};
+
+createIntel(intel);
+createAMD(ryzen);
